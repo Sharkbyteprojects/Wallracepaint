cf43700af924833b83e317323a99535c9eb891b3
-- <-->
2019-03-04 13:29:25 +0100

    NEW

---
+++ Scripts/WIN.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WIN : MonoBehaviour
+{
+    public GameObject destroyi;
+    public float speed = 3.0f;
+    public float speedup = 3.0f;
+    void Start()
+    {
+        destroyi.SetActive(false);
+    }
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            transform.Translate(Vector3.right * speed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * speed);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(Vector3.up * speedup);
+        }
+       
+        
+    }
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.name == "Ghost")
+        {
+            Debug.Log("WINNER");
+            destroyi.SetActive(true);
+        }
+    }
+}
